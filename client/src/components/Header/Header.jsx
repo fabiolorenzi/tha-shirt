7383ae76b626c6f8e45ca50270cc85d6bbda4772
fabiolorenzi <fabiolorenzi@gmail.com>
2021-06-26 20:29:08 +0100

    Header component compiled and tested

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import "./stylesheets/Header.css";
+
+import LoginButton from "./LoginButton.jsx";
 
 function Header() {
     return(
-        <div className="header">
-            <h1>Hello World!!!</h1>
+        <div className="header" data-testid="headerContainer">
+            <LoginButton />
         </div>
     );
 };
