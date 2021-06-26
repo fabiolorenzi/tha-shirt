5a8ece4160eb7e8b092b85ee063460af4e51260b
fabiolorenzi <fabiolorenzi@gmail.com>
2021-06-26 21:39:25 +0100

    styled more LoginButton and Header components

--- client/src/components/Header/LoginButton.jsx
+++ client/src/components/Header/LoginButton.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import notLogged from "../../img/unlogged-icon.png";
 import logged from "../../img/logged-icon.png";
 import "./stylesheets/LoginButton.css";
 
 function LoginButton() {
+    // eslint-disable-next-line
     const [login, setLogin] = useState({
         logged: false,
         username: ""
@@ -12,7 +13,7 @@ function LoginButton() {
 
     return(
         <div className="loginButtonContainer">
-            <Link to="/loginArea">
+            <Link to={login.logged ? "/account/:id" : "/login"}>
                 {!login.logged
                     ?
                         <div className="login_button">
