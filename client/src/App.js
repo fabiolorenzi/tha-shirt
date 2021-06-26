f9019543e8f2fbe5fc709b41d43659a99d9e15af
fabiolorenzi <fabiolorenzi@gmail.com>
2021-06-26 17:41:18 +0100

    created App into client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+function App() {
+    return(
+        <div className="app">
+            <h1>Hello World</h1>
+            <Switch>
+            </Switch>
+        </div>
+    );
+};
+
+export default App;
