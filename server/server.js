17b25f493e38a99b1c2f53772b08dd5bce458f30
fabiolorenzi <fabiolorenzi@gmail.com>
2021-06-26 16:00:57 +0100

    implemented server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const connectDB = require("./config/db");
+var cors = require("cors");
+
+const products = require("./routes/api/products");
+const users = require("./routes/api/users");
+const orders = require("./routes/api/orders");
+
+const app = express();
+
+connectDB();
+
+app.use(cors({origin: true, credentials: true}));
+app.use(express.join({extended: false}));
+app.get("/", (req, res) => res.send("The server is working"));
+app.use("/api/products", products);
+app.use("/api/users", users);
+app.use("/api/orders", orders);
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log("Server is running on port " + port));
