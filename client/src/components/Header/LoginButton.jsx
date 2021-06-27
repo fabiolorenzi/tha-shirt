391bee0d25dec111f74ac4ce1495808fbf863d26
fabiolorenzi <fabiolorenzi@gmail.com>
2021-06-27 08:39:53 +0100

    finished and tested LoginButton component

--- client/src/components/Header/LoginButton.jsx
+++ client/src/components/Header/LoginButton.jsx
@@ -1,27 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import notLogged from "../../img/unlogged-icon.png";
 import logged from "../../img/logged-icon.png";
 import "./stylesheets/LoginButton.css";
 
 function LoginButton() {
-    // eslint-disable-next-line
     const [login, setLogin] = useState({
         logged: false,
         username: ""
     });
 
+    useEffect(() => {
+        if (localStorage.getItem("logged") === true) {
+            setLogin({
+                logged: true,
+                username: localStorage.getItem("username")
+            });
+        }
+    }, []);
+
     return(
-        <div className="loginButtonContainer">
+        <div className="loginButtonContainer" data-testid="loginButtonContainer">
             <Link to={login.logged ? "/account/:id" : "/login"}>
                 {!login.logged
                     ?
-                        <div className="login_button">
+                        <div className="login_button" data-testid="button">
                             <h3 data-testid="username">Not Logged</h3>
                             <img src={notLogged} alt="not logged icon" />
                         </div>
                     :
-                        <div className="login_button">
+                        <div className="login_button" data-testid="button">
                             <h3 data-testid="username">{login.username}</h3>
                             <img src={logged} alt="logged icon" />
                         </div>
