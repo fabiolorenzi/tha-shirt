6feda05390910d054195b11300ef6ffc6e57cc12
fabiolorenzi <fabiolorenzi@gmail.com>
2021-06-27 10:07:59 +0100

    improved LanguageSelection logic

--- client/src/components/Header/LanguageSelection.jsx
+++ client/src/components/Header/LanguageSelection.jsx
@@ -2,20 +2,14 @@ import React, { useEffect, useState } from "react";
 import "./stylesheets/LanguageSelection.css";
 
 function LanguageSelection() {
-    const [lang, setLang] = useState("eng");
-
     const langSel = e => {
         e.preventDefault();
-        setLang(e.target.value)
+        localStorage.setItem("lang", e.target.value);
     };
-
-    useEffect(() => {
-        localStorage.setItem("lang", lang);
-    }, [lang]);
 
     return(
         <div className="languageSelectionContainer">
-            <select onChange={langSel} defaultValue="eng">
+            <select onChange={langSel} defaultValue={localStorage.getItem("lang") ? localStorage.getItem("lang") : "eng"}>
                 <option value="eng">English</option>
                 <option value="ita">Italiano</option>
             </select>
