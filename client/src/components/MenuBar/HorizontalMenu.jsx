cea16d3686867b8c0eb456414dbc6dabb0adb0ef
fabiolorenzi <fabiolorenzi@gmail.com>
2021-06-28 21:06:29 +0100

    error solved in HorizontalMenu component

--- client/src/components/MenuBar/HorizontalMenu.jsx
+++ client/src/components/MenuBar/HorizontalMenu.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./stylesheets/HorizontalMenu.css";
 
 function HorizontalMenu() {
     return(
         <div className="horizontalMenuContainer" data-testid="horizontalMenuContainer">
-            <NavLink to="/" className="horiMenu1" activeClassMenu="activeHoriMenu">HOME</NavLink>
-            <NavLink to="/shop" className="horiMenu1" activeClassMenu="activeHoriMenu">SHOP</NavLink>
-            <NavLink to="/aboutus" className="horiMenu1" activeClassMenu="activeHoriMenu">ABOUT US</NavLink>
-            <NavLink to="/contacts" className="horiMenu1" activeClassMenu="activeHoriMenu">CONTACTS</NavLink>
-            <NavLink to="/managment" className="horiMenu1" activeClassMenu="activeHoriMenu">MANAGMENT</NavLink>
+            <NavLink to="/" className="horiMenu1" activeClassName="activeHoriMenu">HOME</NavLink>
+            <NavLink to="/shop" className="horiMenu1" activeClassName="activeHoriMenu">SHOP</NavLink>
+            <NavLink to="/aboutus" className="horiMenu1" activeClassName="activeHoriMenu">ABOUT US</NavLink>
+            <NavLink to="/contacts" className="horiMenu1" activeClassName="activeHoriMenu">CONTACTS</NavLink>
+            <NavLink to="/managment" className="horiMenu1" activeClassName="activeHoriMenu">MANAGMENT</NavLink>
         </div>
     );
 };
