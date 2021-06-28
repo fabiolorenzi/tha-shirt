0321e36ac195ff30949ee37e7ff1b978cce788ff
fabiolorenzi <fabiolorenzi@gmail.com>
2021-06-28 08:15:27 +0100

    created CurrencySelection component

---
+++ client/src/components/Header/CurrencySelection.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./stylesheets/CurrencySelection.css";
+
+function CurrencySelection() {
+    const currencySel = e => {
+        e.preventDefault();
+        localStorage.setItem("currency", e.target.value);
+    };
+
+    return(
+        <div className="currencySelectionContainer" data-testid="currencySelectionContainer">
+            <select
+                className="currencySelector"
+                data-testid="selector"
+                defaultValue={localStorage.getItem("currency") ? localStorage.getItem("currency") : "£"}
+                onChange={currencySel}
+            >
+                <option data-testid="option1" val="£">£</option>
+                <option data-testid="option2" val="€">€</option>
+                <option data-testid="option3" val="$">$</option>
+            </select>
+        </div>
+    );
+};
+
+export default CurrencySelection;
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,12 +3,14 @@ import "./stylesheets/Header.css";
 
 import LoginButton from "./LoginButton.jsx";
 import LanguageSelection from "./LanguageSelection.jsx";
+import CurrencySelection from "./CurrencySelection.jsx";
 
 function Header() {
     return(
         <div className="header" data-testid="headerContainer">
             <LoginButton />
             <LanguageSelection />
+            <CurrencySelection />
         </div>
     );
 };
