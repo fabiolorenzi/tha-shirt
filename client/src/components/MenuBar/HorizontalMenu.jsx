194878528dcb1af521f490d5400e5110adb488d3
fabiolorenzi <fabiolorenzi@gmail.com>
2021-06-28 21:48:56 +0100

    added logic to HorizontalMenu component

--- client/src/components/MenuBar/HorizontalMenu.jsx
+++ client/src/components/MenuBar/HorizontalMenu.jsx
@@ -3,13 +3,15 @@ import { NavLink } from "react-router-dom";
 import "./stylesheets/HorizontalMenu.css";
 
 function HorizontalMenu() {
+    const pass = localStorage.getItem("pass") ? "" : "reservedHoriMenu";
+
     return(
         <div className="horizontalMenuContainer" data-testid="horizontalMenuContainer">
             <NavLink to="/" exact className="horiMenu1" activeClassName="activeHoriMenu">HOME</NavLink>
             <NavLink to="/shop" className="horiMenu1" activeClassName="activeHoriMenu">SHOP</NavLink>
             <NavLink to="/aboutus" className="horiMenu1" activeClassName="activeHoriMenu">ABOUT US</NavLink>
             <NavLink to="/contacts" className="horiMenu1" activeClassName="activeHoriMenu">CONTACTS</NavLink>
-            <NavLink to="/managment" className="horiMenu1" activeClassName="activeHoriMenu" id="reservedHoriMenu">MANAGMENT</NavLink>
+            <NavLink to="/managment" className="horiMenu1" activeClassName="activeHoriMenu" id={pass}>MANAGMENT</NavLink>
         </div>
     );
 };
