374b394d7bfc77fdef801a6e4515b330ae3db8b9
fabiolorenzi <fabiolorenzi@gmail.com>
2021-06-28 17:39:12 +0100

    created MenuBar component

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import Header from "./components/Header/Header.jsx";
+import MenuBar from "./components/MenuBar/MenuBar.jsx";
 
 function App() {
     return(
         <div className="app">
             <Header />
+            <MenuBar />
             <Switch>
             </Switch>
         </div>
---
+++ client/src/components/MenuBar/MenuBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./stylesheets/MenuBar.css";
+
+function MenuBar() {
+    return(
+        <div className="menuBarContainer" data-testid="menuBarContainer">
+        </div>
+    );
+};
+
+export default MenuBar;
