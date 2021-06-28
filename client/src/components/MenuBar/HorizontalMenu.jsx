243d2e2264567cfeaa7ec7a7686875ad9c0390d3
fabiolorenzi <fabiolorenzi@gmail.com>
2021-06-28 21:38:50 +0100

    styled HorizontalMenu component

--- client/src/components/MenuBar/HorizontalMenu.jsx
+++ client/src/components/MenuBar/HorizontalMenu.jsx
@@ -5,11 +5,11 @@ import "./stylesheets/HorizontalMenu.css";
 function HorizontalMenu() {
     return(
         <div className="horizontalMenuContainer" data-testid="horizontalMenuContainer">
-            <NavLink to="/" className="horiMenu1" activeClassName="activeHoriMenu">HOME</NavLink>
+            <NavLink to="/" exact className="horiMenu1" activeClassName="activeHoriMenu">HOME</NavLink>
             <NavLink to="/shop" className="horiMenu1" activeClassName="activeHoriMenu">SHOP</NavLink>
             <NavLink to="/aboutus" className="horiMenu1" activeClassName="activeHoriMenu">ABOUT US</NavLink>
             <NavLink to="/contacts" className="horiMenu1" activeClassName="activeHoriMenu">CONTACTS</NavLink>
-            <NavLink to="/managment" className="horiMenu1" activeClassName="activeHoriMenu">MANAGMENT</NavLink>
+            <NavLink to="/managment" className="horiMenu1" activeClassName="activeHoriMenu" id="reservedHoriMenu">MANAGMENT</NavLink>
         </div>
     );
 };
