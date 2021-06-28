c34a8a93a5210c73d957542a3e4694c9a8bd427f
fabiolorenzi <fabiolorenzi@gmail.com>
2021-06-28 22:07:02 +0100

    HorizontalMenu component tested

--- client/src/components/MenuBar/HorizontalMenu.jsx
+++ client/src/components/MenuBar/HorizontalMenu.jsx
@@ -7,11 +7,11 @@ function HorizontalMenu() {
 
     return(
         <div className="horizontalMenuContainer" data-testid="horizontalMenuContainer">
-            <NavLink to="/" exact className="horiMenu1" activeClassName="activeHoriMenu">HOME</NavLink>
-            <NavLink to="/shop" className="horiMenu1" activeClassName="activeHoriMenu">SHOP</NavLink>
-            <NavLink to="/aboutus" className="horiMenu1" activeClassName="activeHoriMenu">ABOUT US</NavLink>
-            <NavLink to="/contacts" className="horiMenu1" activeClassName="activeHoriMenu">CONTACTS</NavLink>
-            <NavLink to="/managment" className="horiMenu1" activeClassName="activeHoriMenu" id={pass}>MANAGMENT</NavLink>
+            <NavLink to="/" exact className="horiMenu1" activeClassName="activeHoriMenu" data-testid="link1">HOME</NavLink>
+            <NavLink to="/shop" className="horiMenu1" activeClassName="activeHoriMenu" data-testid="link2">SHOP</NavLink>
+            <NavLink to="/aboutus" className="horiMenu1" activeClassName="activeHoriMenu" data-testid="link3">ABOUT US</NavLink>
+            <NavLink to="/contacts" className="horiMenu1" activeClassName="activeHoriMenu" data-testid="link4">CONTACTS</NavLink>
+            <NavLink to="/managment" className="horiMenu1" activeClassName="activeHoriMenu" id={pass} data-testid="link5">MANAGMENT</NavLink>
         </div>
     );
 };
