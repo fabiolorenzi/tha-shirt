99fc305dc1918f35ec72cdb2cfca04d97ecb57c9
fabiolorenzi <fabiolorenzi@gmail.com>
2021-06-29 22:34:06 +0100

    tested ImageRoller component

--- client/src/components/Home/ImageRoller.jsx
+++ client/src/components/Home/ImageRoller.jsx
@@ -13,25 +13,25 @@ function ImageRoller() {
     return(
         <div className="imageRollerContainer" data-testid="imageRollerContainer">
             <AliceCarousel autoPlay autoPlayInterval="4000" infinite="true">
-                <div className="sliderimg">
-                    <h1>All our shops soon open! Stay ready!</h1>
+                <div className="sliderimg" data-testid="firstDiv">
+                    <h1 data-testid="firstText">All our shops soon open! Stay ready!</h1>
                     <img src={Image1} alt="first" />
                 </div>
-                <div className="sliderimg">
-                    <h1>Summer discount! Enjoy the best time of the year</h1>
-                    <img src={Image2} alt="first" />
+                <div className="sliderimg" data-testid="secondDiv">
+                    <h1 data-testid="secondText">Summer discount! Enjoy the best time of the year</h1>
+                    <img src={Image2} alt="second" />
                 </div>
-                <div className="sliderimg">
-                    <h1>All the best clothes for the best parties!</h1>
-                    <img src={Image3} alt="first" />
+                <div className="sliderimg" data-testid="thirdDiv">
+                    <h1 data-testid="thirdText">All the best clothes for the best parties!</h1>
+                    <img src={Image3} alt="third" />
                 </div>
-                <div className="sliderimg">
-                    <h1>Be quick! The special discount is just for this July!</h1>
-                    <img src={Image4} alt="first" />
+                <div className="sliderimg" data-testid="fourthDiv">
+                    <h1 data-testid="fourthText">Be quick! The special discount is just for this July!</h1>
+                    <img src={Image4} alt="fourth" />
                 </div>
-                <div className="sliderimg">
-                    <h1>Clothes for every occasion!</h1>
-                    <img src={Image5} alt="first" />
+                <div className="sliderimg" data-testid="fifthDiv">
+                    <h1 data-testid="fifthText">Clothes for every occasion!</h1>
+                    <img src={Image5} alt="fifth" />
                 </div>
             </AliceCarousel>
         </div>
