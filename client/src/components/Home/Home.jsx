d8a56f6ac0f0834d143cd686bdc1a9230190fc3e
fabiolorenzi <fabiolorenzi@gmail.com>
2021-06-29 09:33:44 +0100

    styled Home component and created open burger effect

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import "./stylesheets/Home.css";
 
 function Home() {
+    const burgerButtonState = useSelector(state => state.burgerButtonState);
     const [delThr, setDelThr] = useState();
     const currency = localStorage.getItem("currency") || "£";
 
@@ -15,8 +17,10 @@ function Home() {
         };
     }, [currency]);
 
+    const openBurger = burgerButtonState ? "openHome" : "";
+
     return(
-        <div className="homeContainer">
+        <div className="homeContainer" id={openBurger}>
             <h3>Free delivery from {currency}{delThr}!</h3>
         </div>
     );
