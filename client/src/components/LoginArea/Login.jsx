c611f72d4e5a403284f847aa6c9d379990b5b3ed
fabiolorenzi <fabiolorenzi@gmail.com>
2021-07-03 20:52:35 +0100

    cleaned Login component

--- client/src/components/LoginArea/Login.jsx
+++ client/src/components/LoginArea/Login.jsx
@@ -58,45 +58,6 @@ function Login() {
         });
     };
 
-    /*const loginAction = e => {
-        e.preventDefault();
-        axios.get("http://localhost:8082/api/users/")
-            .then(res => setUsers(res.data))
-            .then(users.forEach(user => {
-                if (user.email === loginData.email) {
-                    i = 0;
-                    if (user.password === loginData.password) {
-                        localStorage.setItem("logged", true);
-                        localStorage.setItem("username", user.username);
-                        localStorage.setItem("pass", user.pass);
-                        localStorage.setItem("id", user._id);
-                        alert("Login successful!!!");
-                        history.push("/");
-                        window.location.reload();
-                    } else {
-                        setLoginData({...loginData, password: ""});
-                        alert("Password not correct! Please check the email you inserted.");
-                    };
-                } else if (i < users.length - 1) {
-                    i++;
-                } else {
-                    alert("User not found! Please check the email you inserted or create the account.");
-                    setLoginData({
-                        email: "",
-                        password: ""
-                    });
-                    i = 0;
-                };
-            }))
-            .catch(err => {
-                alert("Error during login, please try again.");
-                setLoginData({
-                    email: "",
-                    password: ""
-                });
-            });
-    };*/
-
     const psswShow = e => {
         e.preventDefault();
         if (show.showP) {
