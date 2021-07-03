31e008681335536760d411197ba94c12ce51d36d
fabiolorenzi <fabiolorenzi@gmail.com>
2021-07-03 17:42:20 +0100

    styled CreateAccount component

--- client/src/components/LoginArea/CreateAccount.jsx
+++ client/src/components/LoginArea/CreateAccount.jsx
@@ -70,40 +70,74 @@ function CreateAccount() {
         <div className="createAccountContainer">
             <h1>Create account</h1>
             <form onSubmit={createUser}>
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" value={user.name} onChange={handleChange} />
-                <label htmlFor="surname">Surname</label>
-                <input type="text" name="surname" value={user.surname} onChange={handleChange} />
-                <label htmlFor="username">Username</label>
-                <input type="text" name="username" value={user.username} onChange={handleChange} />
-                <label htmlFor="birthday">Birthday</label>
-                <input type="date" name="birthday" value={user.birthday} onChange={handleChange} />
-                <label htmlFor="gender">Gender</label>
-                <div className="genderSelector" onChange={handleChange}>
-                    <input type="radio" name="gender" value="male" />
-                    <label>Male</label>
-                    <input type="radio" name="gender" value="female" />
-                    <label>Female</label>
-                    <input type="radio" name="gender" value="not-binary" />
-                    <label>Not Binary</label>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" value={user.name} onChange={handleChange} />
                 </div>
-                <label htmlFor="nation">Nation</label>
-                <input type="text" name="nation" value={user.nation} onChange={handleChange} />
-                <label htmlFor="city">City</label>
-                <input type="text" name="city" value={user.city} onChange={handleChange} />
-                <label htmlFor="street">Street</label>
-                <input type="text" name="street" value={user.street} onChange={handleChange} />
-                <label htmlFor="number">Number</label>
-                <input type="text" name="number" value={user.number} onChange={handleChange} />
-                <label htmlFor="postal_code">Postal Code</label>
-                <input type="text" name="postal_code" value={user.postal_code} onChange={handleChange} />
-                <label htmlFor="tel">Tel</label>
-                <input type="tel" name="tel" value={user.tel} onChange={handleChange} />
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" value={user.email} onChange={handleChange} />
-                <label htmlFor="password">Password</label>
-                <input type="text" name="password" value={user.password} onChange={handleChange} />
-                <button type="submit">Send</button>
+                <div>
+                    <label htmlFor="surname">Surname</label>
+                    <input type="text" name="surname" value={user.surname} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" name="username" value={user.username} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="birthday">Birthday</label>
+                    <input type="date" name="birthday" value={user.birthday} onChange={handleChange} />
+                </div>
+                <div className="radiusSelector">
+                    <h3 id="genderLabel">Gender</h3>
+                    <div className="genderSelector" onChange={handleChange}>
+                        <div>
+                            <input type="radio" name="gender" value="male" className="radInp" />
+                            <p className="radLab">Male</p>
+                        </div>
+                        <div>
+                            <input type="radio" name="gender" value="female" className="radInp" />
+                            <p className="radLab">Female</p>
+                        </div>
+                        <div>
+                            <input type="radio" name="gender" value="not-binary" className="radInp" />
+                            <p className="radLab">Not Binary</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <label htmlFor="nation">Nation</label>
+                    <input type="text" name="nation" value={user.nation} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="city">City</label>
+                    <input type="text" name="city" value={user.city} onChange={handleChange} />
+                </div>             
+                <div>
+                    <label htmlFor="street">Street</label>
+                    <input type="text" name="street" value={user.street} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="number">Number</label>
+                    <input type="text" name="number" value={user.number} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="postal_code">Postal Code</label>
+                    <input type="text" name="postal_code" value={user.postal_code} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="tel">Tel</label>
+                    <input type="tel" name="tel" value={user.tel} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" value={user.email} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="text" name="password" value={user.password} onChange={handleChange} />
+                </div>
+                <div>
+                    <button type="submit">Send</button>
+                </div>          
             </form>
         </div>
     );
