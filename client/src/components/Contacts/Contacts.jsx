3249a39c35fa917aade7f97e0a0ba8edc20f3c76
fabiolorenzi <fabiolorenzi@gmail.com>
2021-07-05 18:22:27 +0100

    added icons in Contacts component

--- client/src/components/Contacts/Contacts.jsx
+++ client/src/components/Contacts/Contacts.jsx
@@ -3,6 +3,10 @@ import { useSelector } from "react-redux";
 import { Helmet } from "react-helmet";
 import "./stylesheets/Contacts.css";
 
+import { FaFacebookSquare, FaInstagramSquare } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { ImLinkedin } from "react-icons/im";
+
 import Logo from "../../img/tha-shirt-logo.png";
 
 function Contacts() {
@@ -22,6 +26,12 @@ function Contacts() {
                 </div>
                 <div className="contactsRight" data-testid="contactsRight">
                     <h3 data-testid="contactsRightTitle">Contacts us on:</h3>
+                    <div className="contactsLinks" data-testid="contactsLinks">
+                        <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookSquare /></a>
+                        <a href="https://instagram.com" target="_blank" rel="noreferrer"><FaInstagramSquare /></a>
+                        <a href="mailto:fabiolorenzi13975@gmail.com" target="_blank" rel="noreferrer"><MdEmail /></a>
+                        <a href="https://www.linkedin.com/in/fabio-lorenzi-6901421b3/" target="_blank" rel="noreferrer"><ImLinkedin /></a>
+                    </div>
                 </div>
             </div>
         </div>
