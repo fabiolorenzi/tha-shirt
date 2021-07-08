026c932934c90ebf1ead22202b5a592832b97709
fabiolorenzi <fabiolorenzi@gmail.com>
2021-07-08 17:08:47 +0100

    removed redux thunk

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,14 +1,13 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore } from "redux";
 import rootReducer from "./reducers/rootReducer.js";
-import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 let store;
 
 if (process.env.NODE_ENV === "production") {
-    store = createStore(rootReducer, applyMiddleware(thunk));
+    store = createStore(rootReducer);
 } else {
-    store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+    store = createStore(rootReducer, composeWithDevTools());
 };
 
 export default store;
