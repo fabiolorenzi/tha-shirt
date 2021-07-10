056e5b56622531efd4233ff9c12c83b5c9fab655
fabiolorenzi <fabiolorenzi@gmail.com>
2021-07-10 09:48:37 +0100

    created logic to get data in SearchList component

--- client/src/components/Shop/SearchList.jsx
+++ client/src/components/Shop/SearchList.jsx
@@ -9,6 +9,21 @@ import Product from "./Product.jsx";
 function SearchList() {
     const searchWord = useSelector(state => state.searchWord);
 
+    const [listItems, setListItems] = useState([]);
+    let all = [];
+
+    useEffect(() => {
+        axios.get("http://localhost:8082/api/products")
+            .then(res => all.push(res.data))
+            .catch(err => console.log(err));
+        for (let i = 0; i < all.length; i++) {
+            if (all[i].name.includes(searchWord)) {
+                setListItems(listItems => [...listItems, all[i]]);
+            };
+        };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return(
         <div className="searchListContainer">
             <h1>You searched: {searchWord}</h1>
